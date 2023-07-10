442f52838bcc4744e5836a3f7ed7f42212ddc66a
3juhwan <13selfesteem91@naver.com>
2023-07-10 23:29:29 +0900

    Java Study

---
+++ BOJ/14581.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + br.readLine() + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
