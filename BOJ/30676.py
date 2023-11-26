e8071663216ed1987b2aa342282f3df64395c1a7
3juhwan <13selfesteem91@naver.com>
2023-11-26 23:47:50 +0900

    solved BOJ30676 by python

---
+++ BOJ/30676.py
@@ -0,0 +1,15 @@
+n = int(input())
+if 620 <= n:
+    print("Red")
+elif 590 <= n:
+    print("Orange")
+elif 570 <= n:
+    print("Yellow")
+elif 495 <= n:
+    print("Green")
+elif 450 <= n:
+    print("Blue")
+elif 425 <= n:
+    print("Indigo")
+elif 380 <= n:
+    print("Violet")
